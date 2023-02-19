859f32c7dedb6b49f8bcc84e4d2d98c2d8518472
Shantanu <77541968+shantanu1998s@users.noreply.github.com>
2023-02-19 20:27:43 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import '../styles/App.css';
 
-const App = () => {
+const App = ({ slides }) => {
+  const [currIdx, setIndex] = useState(0);
+
   return (
-    <></>
+    <>
+      <div>
+        <h1 data-testid="title">{slides[currIdx].title}</h1>
+        <p data-testid="text">{slides[currIdx].text}</p>
+      </div>
+      <button disabled={currIdx == 0} data-testid="button-prev" onClick={() => { setIndex(currIdx - 1) }}>Prev</button>
+      <button disabled={currIdx == 0} data-testid="button-restart" onClick={() => { setIndex(0) }}>Restart</button>
+      <button disabled={currIdx == 4} data-testid="button-next" onClick={() => { setIndex(currIdx + 1) }}>Next</button>
+    </>
   )
 }
-
 
 export default App;
 
